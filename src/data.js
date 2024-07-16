c8189099c9436e022ec30a44039059ca59a0aaab
Dmitry Kuryndin <dimakuryndin1@yandex.ru>
2024-07-16 16:44:06 +0300

    Auto-commit

--- src/data.js
+++ src/data.js
@@ -1,7 +1,8 @@
-const dataNews = [
+export default const dataNews = [
   "<p>ееуу</p>",
   "<p>вапаппп</p>",
   "<p>test</p>",
   "<p>tesre333</p>",
-  "<p>wersdfsdf</p>"
+  "<p>wersdfsdf</p>",
+  "<p>teeeeeee</p>"
 ];
