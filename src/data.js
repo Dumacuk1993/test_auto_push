1096e026e18ce59945894db66a65457365e3a2d5
Dmitry Kuryndin <dimakuryndin1@yandex.ru>
2024-07-17 12:59:36 +0300

    Auto-commit

--- src/data.js
+++ src/data.js
@@ -10,6 +10,9 @@ const dataNews = [
   },
   {
     "html_template": "<p><img src=\"https://ya.ru/images/search?from=tabbar&amp;img_url=https%3A%2F%2Fi.artfile.ru%2F3728x2481_789397_%5Bwww.ArtFile.ru%5D.jpg&amp;lr=20728&amp;pos=9&amp;rpt=simage&amp;text=%D0%BA%D0%B0%D1%80%D1%82%D0%B8%D0%BD%D0%BA%D0%B8\" alt=\"\"></p>"
+  },
+  {
+    "html_template": "<p><img src=\"https://ya.ru/images/search?from=tabbar&amp;img_url=https%3A%2F%2Fi.artfile.ru%2F3728x2481_789397_%5Bwww.ArtFile.ru%5D.jpg&amp;lr=20728&amp;pos=9&amp;rpt=simage&amp;text=%D0%BA%D0%B0%D1%80%D1%82%D0%B8%D0%BD%D0%BA%D0%B8\" alt=\"\">dfgvcxbcvb</p><p><br></p><p>ghklljk</p><p><br></p><p>jkljklkjj;lk</p><p><br></p><p>jkljklkj</p><p><br></p><p><br></p>"
   }
 ];
     
