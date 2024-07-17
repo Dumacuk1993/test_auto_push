5f6114fadc0c481aa8263da757f143e1178e56f6
Dmitry Kuryndin <dimakuryndin1@yandex.ru>
2024-07-17 12:51:08 +0300

    Auto-commit

--- src/data.js
+++ src/data.js
@@ -4,6 +4,9 @@ const dataNews = [
   },
   {
     "html_template": "<p>ыав<img src=\"https://dabbar.ru/files/img/photos/kartinki-na-rabochii-stol-v-formate-hd/kartinki-na-rabochii-stol-v-formate-hd-1.webp\" alt=\"\"></p><p><br></p><p>gfhdfgdfg</p><p>xgg</p>"
+  },
+  {
+    "html_template": "<p><img src=\"https://get.wallhere.com/photo/Trey-Ratcliff-photography-cityscape-France-Paris-Eiffel-Tower-2196341.jpg\" alt=\"\"></p><p><br></p><p><br></p><p><img src=\"https://get.wallhere.com/photo/Trey-Ratcliff-photography-cityscape-France-Paris-Eiffel-Tower-2196341.jpg\" alt=\"\"></p><p><br></p><p>sdfsf</p><p><br></p><p><img src=\"https://ya.ru/images/search?from=tabbar&amp;img_url=https%3A%2F%2Fblog.centraldereservas.com%2Fwp-content%2Fuploads%2F2014%2F09%2Fsan-francisco.jpg&amp;lr=20728&amp;pos=6&amp;rpt=simage&amp;text=%D0%BA%D0%B0%D1%80%D1%82%D0%B8%D0%BD%D0%BA%D0%B8\" alt=\"\"></p><p><img src=\"https://ya.ru/images/search?from=tabbar&amp;img_url=https%3A%2F%2Fblog.centraldereservas.com%2Fwp-content%2Fuploads%2F2014%2F09%2Fsan-francisco.jpg&amp;lr=20728&amp;pos=6&amp;rpt=simage&amp;text=%D0%BA%D0%B0%D1%80%D1%82%D0%B8%D0%BD%D0%BA%D0%B8\" alt=\"\">dfgdfgdfgdfgdfgfdgfdgfdgdfgdfgfdgfd</p><p><br></p><p><img src=\" https://ya.ru/images/search?from=tabbar&amp;img_url=https%3A%2F%2Fi.artfile.ru%2F3728x2481_789397_%5Bwww.ArtFile.ru%5D.jpg&amp;lr=20728&amp;pos=9&amp;rpt=simage&amp;text=%D0%BA%D0%B0%D1%80%D1%82%D0%B8%D0%BD%D0%BA%D0%B8\" alt=\"\"></p>"
   }
 ];
     
