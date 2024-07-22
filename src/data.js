00274dee39eaa58c5f4ec20e1f64cf20916fd622
Dmitry Kuryndin <dimakuryndin1@yandex.ru>
2024-07-22 11:07:41 +0300

    Auto-commit

--- src/data.js
+++ src/data.js
@@ -1,12 +1,5 @@
 const dataNews = [
   {
-    "html_template": "<p><img src=\"https://img1.akspic.ru/attachments/crops/1/4/9/1/3/131941/131941-banff-park-pustynya-nacionalnyj_park-gornyj_relef-2560x1440.jpg\" alt=\"\"></p><p>Абзац1</p><p>Абзац2</p><h2>Подзаголовок 1</h2><p>Абзац 3</p><p><img src=\"https://get.wallhere.com/photo/2560x1600-px-clear-sky-forest-landscape-pine-trees-road-sky-summer-1413157.jpg\" alt=\"\"></p>",
-    "date": "17.07.2024",
-    "logo": "",
-    "title": "Заголов статьи 1",
-    "image": "https://img1.akspic.ru/attachments/crops/1/4/9/1/3/131941/131941-banff-park-pustynya-nacionalnyj_park-gornyj_relef-2560x1440.jpg"
-  },
-  {
     "id": 1721635559230,
     "html_template": "<p><img src=\"https://img1.akspic.ru/attachments/crops/1/4/9/1/3/131941/131941-banff-park-pustynya-nacionalnyj_park-gornyj_relef-2560x1440.jpg\"></p><p>Абзац1</p><p>Абзац2</p><p><br></p><h2>Подзаголово324234</h2><p>Абзац 3</p><p><img src=\"https://get.wallhere.com/photo/2560x1600-px-clear-sky-forest-landscape-pine-trees-road-sky-summer-1413157.jpg\"></p>",
     "date": "22.07.2024",
