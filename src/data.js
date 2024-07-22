9ea2d8820f4bcffbfddb1870db1f8e4c79fe1270
Dmitry Kuryndin <dimakuryndin1@yandex.ru>
2024-07-22 15:16:31 +0300

    Auto-commit

--- src/data.js
+++ src/data.js
@@ -1,13 +1,5 @@
 const dataNews = [
   {
-    "id": 1721635559230,
-    "html_template": "<p><img src=\"https://img1.akspic.ru/attachments/crops/1/4/9/1/3/131941/131941-banff-park-pustynya-nacionalnyj_park-gornyj_relef-2560x1440.jpg\"></p><p>Абзац1</p><p>Абзац2</p><h2>Подзаголово324234</h2><p>Абзац 3</p>",
-    "date": "22.07.2024",
-    "logo": "",
-    "title": "Заголов статьи 1",
-    "image": "https://img1.akspic.ru/attachments/crops/1/4/9/1/3/131941/131941-banff-park-pustynya-nacionalnyj_park-gornyj_relef-2560x1440.jpg"
-  },
-  {
     "id": 1721635600820,
     "html_template": "<p><img src=\"https://img1.akspic.ru/attachments/crops/1/4/9/1/3/131941/131941-banff-park-pustynya-nacionalnyj_park-gornyj_relef-2560x1440.jpg\"></p><p>Абзац1</p><p>Абзац2</p><p><br></p><h2>Подзаголовок 1</h2><p>Абзац 3</p><p><br></p>",
     "date": "22.07.2024",
