68a1185f61a1cedc9b4021f9ff63f113bbe93e17
Dmitry Kuryndin <dimakuryndin1@yandex.ru>
2024-07-22 16:01:54 +0300

    Auto-commit

--- src/data.js
+++ src/data.js
@@ -1,3 +1,12 @@
-const dataNews = [];
+const dataNews = [
+  {
+    "id": "d5b862f9-4073-444d-9554-422c9cfa4c72",
+    "html_template": "<p>fhfghfgh</p>",
+    "date": "22.07.2024",
+    "logo": "",
+    "title": "fghfgh",
+    "image": ""
+  }
+];
 
 export default dataNews;
