8bf536e44d599fb05d5c05ac02675db0c24d0581
Dmitry Kuryndin <dimakuryndin1@yandex.ru>
2024-08-05 14:09:09 +0300

    Auto-commit

--- src/data.js
+++ src/data.js
@@ -6,6 +6,14 @@ const dataNews = [
     "logo": "ED",
     "title": "fghf",
     "image": "fghfgh"
+  },
+  {
+    "id": "33ccf861-d3ea-4511-b2d4-7f5506fb0893",
+    "html_template": "<p>dfgdfff</p><p>vj</p>",
+    "date": "05.08.2024",
+    "logo": "",
+    "title": "dfhdfgf",
+    "image": ""
   }
 ];
 
